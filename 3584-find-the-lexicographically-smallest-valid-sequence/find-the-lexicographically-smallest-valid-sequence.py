9c1fe3aca30de4abe43fd27bd6d370e48fcd3558
Gavneet Singh <office.gavneetsingh30@gmail.com>
2026-08-08 19:35:19 +0530

    Time: 395 ms (89.13%) | Memory: 46.6 MB (86.96%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
@@ -3,33 +3,31 @@ class Solution:
         n = len(word1)
         m = len(word2)
         last = [-1] * m
-
         i = n - 1
         j = m - 1
-
         while i >= 0 and j >= 0:
             if word1[i] == word2[j]:
                 last[j] = i
                 j -= 1
             i -= 1
-
         ans = [-1] * m
-        canSkip = True
-
+        used_mismatch = False
         j = 0
 
         for i in range(n):
+
             if j == m:
                 break
             if word1[i] == word2[j]:
                 ans[j] = i
                 j += 1
-            elif canSkip and (
+
+            elif not used_mismatch and (
                 j == m - 1 or i < last[j + 1]
             ):
                 ans[j] = i
                 j += 1
-                canSkip = False
+                used_mismatch = True
 
         if j != m:
             return []
