439af4ce2135e0b6851bb4ad155f5eac3f80022f
Gavneet Singh <office.gavneetsingh30@gmail.com>
2026-08-28 22:12:07 +0530

    Time: 43 ms (30.30%) | Memory: 19.5 MB (48.48%) - LeetSync

--- 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.py
+++ 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.py
@@ -6,21 +6,16 @@ class Solution:
         for ch in s:
             cnt[ord(ch) - ord('a')] += 1
 
-        # A palindrome can have at most one odd-count character.
         odd = [i for i in range(26) if cnt[i] % 2]
         if len(odd) > 1:
             return ""
 
         mid = "" if not odd else chr(odd[0] + ord('a'))
-
-        # Only half of the palindrome needs to be constructed.
         half_cnt = [x // 2 for x in cnt]
         h = n // 2
 
         def build(left):
             return left + mid + left[::-1]
-
-        # Try to make the left half equal to target[:h].
         rem = half_cnt[:]
         prefix = []
 
@@ -33,17 +28,13 @@ class Solution:
             rem[c] -= 1
             prefix.append(target[i])
         else:
-            # target's entire left half is possible.
             candidate = build(''.join(prefix))
 
             if candidate > target:
                 return candidate
-
-        # Try changing the rightmost possible position.
         for i in range(h - 1, -1, -1):
             rem = half_cnt[:]
 
-            # Match target prefix [0:i].
             possible = True
 
             for j in range(i):
@@ -60,8 +51,6 @@ class Solution:
 
             current = ord(target[i]) - ord('a')
 
-            # Pick the smallest available character
-            # strictly greater than target[i].
             bigger = -1
 
             for c in range(current + 1, 26):
@@ -76,7 +65,6 @@ class Solution:
 
             left = target[:i] + chr(bigger + ord('a'))
 
-            # Fill the remaining half with smallest characters.
             for c in range(26):
                 left += chr(c + ord('a')) * rem[c]
 
